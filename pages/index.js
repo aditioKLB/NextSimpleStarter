6a4c696146e7b55b84c6fe5b931d130a935f6347
aditioKLB <aditio.pangestu@klb.co.id>
2019-11-07 16:40:56 +0700

    hybrid fetching data (apollo and rest)

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import fetch from 'isomorphic-unfetch';
+import Typography from '@material-ui/core/Typography';
+import withApollo from '../libraries/apollo';
+import checkLoggedIn from '../libraries/checkLoggedIn';
+import redirect from '../libraries/redirect';
 import Fork from '../components/Fork';
 import Todo from '../components/Todo';
 
-const Index = ({ stars }) => (
+const Index = ({ stars, user }) => (
 	<>
+		<Typography variant="h3">{user.name}</Typography>
 		<Fork stars={stars} />
 		<Todo />
 	</>
@@ -16,15 +21,23 @@ Index.defaultProps = {
 };
 
 Index.propTypes = {
-	stars: PropTypes.number
+	stars: PropTypes.number,
+	user: PropTypes.object.isRequired
 };
 
-Index.getInitialProps = async () => {
+Index.getInitialProps = async context => {
 	const res = await fetch(
 		'https://api.github.com/repos/ooade/NextSimpleStarter'
 	);
 	const json = await res.json();
-	return { stars: json.stargazers_count };
+	const { loggedInUser } = await checkLoggedIn(context.apolloClient);
+
+	if (!loggedInUser.user) {
+		// Already signed in? No need to continue.
+		// Throw them back to the main page
+		redirect(context, '/signin');
+	}
+	return { stars: json.stargazers_count, ...loggedInUser };
 };
 
-export default Index;
+export default withApollo(Index);
